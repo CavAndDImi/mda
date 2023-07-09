ded577412873322c203f8b4eb1e952bf09033261
CavAndDImi <andreidumitru333@gmail.com>
2023-07-09 23:50:59 +0300

    'comp'

---
+++ static/Scripturi/script_log.js
@@ -0,0 +1,12 @@
+
+function redir(int){
+    
+    if(int == 1) location.assign("/login")
+    else location.assign("/register")
+}
+function check_wallet(){
+    location.assign(location.pathname + "/wallet")
+}
+function info(){
+    location.assign("/info")
+}
